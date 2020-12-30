f371cfff7b75974f718295dc183c952e846584ed
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-30 13:44:54 +0900

    feat: 전화번호부 레이아웃 생성 및 권한 요청 기능 구현

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.sample
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,9 +16,12 @@ import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
+     val permissions = arrayOf(Manifest.permission.READ_CONTACTS)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkReadPermission()
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
@@ -22,5 +31,20 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+
+    fun checkReadPermission() {
+        if (!isPermitted()) {
+            ActivityCompat.requestPermissions(this, permissions, 99)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun isPermitted() : Boolean {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            return false
+        }
+        return true
     }
 }
