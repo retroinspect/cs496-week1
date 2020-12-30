a1a21a4f3489e6b7458a6934626ac4685b88c57b
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-30 17:02:18 +0900

    feat: 주소록 데이터 불러오기 기능 구현

--- app/src/main/java/com/example/sample/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sample/ui/home/HomeFragment.kt
@@ -17,6 +17,7 @@ class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     var list = mutableListOf<Phone>()
+    var sortText = "asc"
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -31,14 +32,26 @@ class HomeFragment : Fragment() {
     }
 
     fun setList() {
-        getPhoneNumbers()
-        //list.addAll(getPhoneNumbers())
+        list.addAll(getPhoneNumbers())
     }
 
-    fun getPhoneNumbers() {
+    fun getPhoneNumbers() : List<Phone> {
         val list = mutableListOf<Phone>()
         val phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI //전화번호 URI
         //전화번호에서 가져올 컬럼 정의
         val projections = arrayOf(ContactsContract.CommonDataKinds.Phone.CONTACT_ID, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER)
+        //정렬 쿼리 사용
+        val optionSort = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " $sortText"
+        //테이블에서 주소록 데이터 쿼리
+        val cursor = context?.getContentResolver()?.query(phoneUri, projections,null,null, optionSort)
+
+        while(cursor?.moveToNext()?:false) {
+            val id = cursor?.getString(0)
+            val name = cursor?.getString(1)
+            val number = cursor?.getString(2)
+            val phone = Phone(id, name, number)
+            list.add(phone)
+        }
+        return list
     }
 }
