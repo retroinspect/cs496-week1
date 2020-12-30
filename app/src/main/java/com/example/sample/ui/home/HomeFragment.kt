620c3eb17534ff8ea6fab62ebccd380a29184981
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-30 20:39:23 +0900

    fix: 출력 오류로 인한 adapter class 수정

--- app/src/main/java/com/example/sample/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sample/ui/home/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.example.sample.ui.home
 
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,15 +14,15 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sample.R
 import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.*
+import kotlin.collections.ArrayList
 
-data class Phone (val id:String?, val name:String?, val phone:String?)
+data class Phone (val id:String, val name:String, val phone:String)
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-    var list = mutableListOf<Phone>()
     var sortText = "asc"
-    lateinit var adapter:PhoneAdapter
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -31,35 +32,38 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         var root: View = inflater.inflate(R.layout.fragment_home, container, false)
-        root = setList(root)
-        return root
-    }
+        var list = getPhoneNumbers()
 
-    fun setList(root: View) : View {
-        list.addAll(getPhoneNumbers())
-        adapter = PhoneAdapter(list)
+//        val adapter = PhoneAdapter(list)
+
+        val dummyList = arrayListOf(Phone("1", "sanga", "2323232"))
+        val adapter = PhoneAdapter(dummyList)
         val recycler : RecyclerView = root.findViewById(R.id.recycler)
         recycler.adapter = adapter
         recycler.layoutManager = LinearLayoutManager(context)
         return root
     }
 
-    fun getPhoneNumbers() : List<Phone> {
-        val list = mutableListOf<Phone>()
+    fun getPhoneNumbers() : ArrayList<Phone> {
+        val list = ArrayList<Phone>()
         val phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI //전화번호 URI
         //전화번호에서 가져올 컬럼 정의
         val projections = arrayOf(ContactsContract.CommonDataKinds.Phone.CONTACT_ID, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER)
         //정렬 쿼리 사용
         val optionSort = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " $sortText"
         //테이블에서 주소록 데이터 쿼리
-        val cursor = context?.getContentResolver()?.query(phoneUri, projections,null,null, optionSort)
+        val cursorOrNull = context?.getContentResolver()?.query(phoneUri, projections,null,null, optionSort)
+        if (cursorOrNull != null) {
+            val cursor = cursorOrNull
 
-        while(cursor?.moveToNext()?:false) {
-            val id = cursor?.getString(0)
-            val name = cursor?.getString(1)
-            val number = cursor?.getString(2)
-            val phone = Phone(id, name, number)
-            list.add(phone)
+            while (cursor.moveToNext()) {
+                val id = cursor.getString(0)
+                val name = cursor.getString(1)
+                val number = cursor.getString(2)
+                val phone = Phone(id, name, number)
+                Log.i("HomeFragment", name)
+                list.add(phone)
+            }
         }
         return list
     }
--- app/src/main/java/com/example/sample/ui/home/PhoneAdapter.kt
+++ app/src/main/java/com/example/sample/ui/home/PhoneAdapter.kt
@@ -1,40 +1,67 @@
 package com.example.sample.ui.home
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sample.R
-import kotlinx.android.synthetic.main.item_layout.view.*
 
-class PhoneAdapter(val list:List<Phone>) : RecyclerView.Adapter<Holder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_layout, parent, false)
-        return Holder(view)
+class PhoneAdapter(val list:ArrayList<Phone>) : RecyclerView.Adapter<PhoneAdapter.PhoneHolder>() {
+    class PhoneHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textName: TextView = itemView.findViewById(R.id.textName)
+        val textPhone: TextView = itemView.findViewById(R.id.textPhone)
+
+        fun bind(item: Phone) {
+            textName.text = item.name
+            textPhone.text = item.phone
+            Log.i("Adapter",item.name)
+            item.name?.let { Log.i("Adapter", it) }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): PhoneHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.list_item_phone, parent, false)
+                return PhoneHolder(view)
+            }
+        }
+    }
+
+    var data = ArrayList<Phone>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhoneHolder {
+        return PhoneHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: PhoneHolder, position: Int) {
+        val item = data[position]
+        holder.bind(item)
     }
 
     override fun getItemCount(): Int {
-        return list.size
-    }
-
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-        val phone = list[position]
-        holder.setPhone(phone)
+        return data.size
     }
 }
 
+
+/*
 @SuppressLint("MissingPermission")
 class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var mPhone:Phone? = null
+
+init {
+}
 
     fun setPhone(phone:Phone) {
         this.mPhone = phone
         itemView.textName.text = phone.name
         itemView.textPhone.text = phone.phone
     }
-}
+}*/
