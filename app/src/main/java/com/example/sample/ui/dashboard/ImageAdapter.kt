07008b206d50acb76368b529ec96ada040c73b42
kmj990929 <kmj990929@dgist.ac.kr>
2021-01-02 21:06:13 +0900

    feat: Card에 제목이 보이도록 구현

--- app/src/main/java/com/example/sample/ui/dashboard/ImageAdapter.kt
+++ app/src/main/java/com/example/sample/ui/dashboard/ImageAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sample.R
 import java.util.ArrayList
@@ -11,9 +12,11 @@ import java.util.ArrayList
 class ImageAdapter: RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
     class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageItem: ImageView = itemView.findViewById(R.id.image_item)
+        val imageTitleView: TextView = itemView.findViewById(R.id.image_title)
 
         fun bind(item: ImageModel) {
             imageItem.setImageURI(item.uri)
+            imageTitleView.text = item.title
         }
 
         companion object {
