b8ea77cf090508437f1be5e6b0f98827c4d5ad85
kmj990929 <kmj990929@dgist.ac.kr>
2021-01-04 12:24:36 +0900

    feat: dashboard viewmodel과 fragment 분리

--- app/src/main/java/com/example/sample/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/sample/ui/dashboard/DashboardFragment.kt
@@ -16,6 +16,7 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sample.R
@@ -24,14 +25,6 @@ import kotlin.collections.ArrayList
 import kotlin.concurrent.schedule
 
 class DashboardFragment : Fragment() {
-    var projection = arrayOf(
-        MediaStore.Images.Media._ID,
-        MediaStore.Images.Media.DATE_ADDED,
-        MediaStore.Images.Media.TITLE
-    )
-    val selection = null
-    val selectionArgs = null
-    val sortOrder = null
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
@@ -48,54 +41,30 @@ class DashboardFragment : Fragment() {
         val adapter = ImageAdapter()
         val layoutManager = LinearLayoutManager(context)
 
+        dashboardViewModel.getAllImages().observe(viewLifecycleOwner,
+            {
+                it?.let {
+                    adapter.data = it
+                    adapter.setItemClickListener(object : ImageAdapter.ItemClickListener {
+                        val itemClickIntent = Intent(context, ClickImageActivity::class.java)
+
+                        override fun onClick(view: View, position: Int) {
+                            itemClickIntent.putExtra("image_uri", it[position].uri.toString())
+                            itemClickIntent.putExtra("image_title", it[position].title)
+                            Log.i("Log test", "before startActivity")
+                            startActivity(itemClickIntent)
+                            Log.i("Log test", "finish activity")
+                            refreshFragment()
+                        }
+                    })
+                }
+            })
+
         images.adapter = adapter
         images.setHasFixedSize(true)
         images.setLayoutManager(layoutManager)
 
-        val allImages = getAllImages()
-        adapter.data = allImages
-
-        adapter.setItemClickListener(object : ImageAdapter.ItemClickListener {
-            val itemClickIntent = Intent(context, ClickImageActivity::class.java)
-
-            override fun onClick(view: View, position: Int) {
-                itemClickIntent.putExtra("image_uri", allImages[position].uri.toString())
-                itemClickIntent.putExtra("image_title", allImages[position].title)
-                Log.i("Log test", "before startActivity")
-                startActivity(itemClickIntent)
-                Log.i("Log test", "finish activity")
-                refreshFragment()
-            }
-        })
         return root
-    }
-
-    private fun getAllImages(): ArrayList<ImageModel> {
-        var imageList: ArrayList<ImageModel> = ArrayList<ImageModel>()
-        val imageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        val cursorOrNull = context?.contentResolver?.query(
-            imageUri,
-            projection,
-            selection,
-            selectionArgs,
-            sortOrder
-        )
-        if (cursorOrNull != null) {
-            val cursor = cursorOrNull
-            val titleColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.TITLE)
-            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
-
-            while (cursor.moveToNext()) {
-                val id = cursor.getLong(idColumn)
-                val title = cursor.getString(titleColumn)
-                val contentUri: Uri =
-                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-                val imageModel = ImageModel(contentUri, title)
-                imageList.add(imageModel)
-            }
-            cursor.close()
-        }
-        return imageList
     }
 
     fun refreshFragment() {
--- app/src/main/java/com/example/sample/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/sample/ui/dashboard/DashboardViewModel.kt
@@ -1,13 +1,54 @@
 package com.example.sample.ui.dashboard
 
-import androidx.lifecycle.LiveData
+import android.app.Application
+import android.content.ContentUris
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class DashboardViewModel(
+    application: Application
+) : AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    private val context = getApplication<Application>().applicationContext
+    private var allImages : MutableLiveData<ArrayList<ImageModel>> = MutableLiveData()
+    private var images = ArrayList<ImageModel>()
+
+    fun getAllImages() : MutableLiveData<ArrayList<ImageModel>> {
+        var projection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DATE_ADDED,
+            MediaStore.Images.Media.TITLE
+        )
+        val selection = null
+        val selectionArgs = null
+        val sortOrder = null
+
+        val imageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val cursorOrNull = context.contentResolver?.query(
+            imageUri,
+            projection,
+            selection,
+            selectionArgs,
+            sortOrder
+        )
+        if (cursorOrNull != null) {
+            val cursor = cursorOrNull
+            val titleColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.TITLE)
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idColumn)
+                val title = cursor.getString(titleColumn)
+                val contentUri: Uri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                val imageModel = ImageModel(contentUri, title)
+                images.add(imageModel)
+            }
+            cursor.close()
+        }
+        allImages.setValue(images)
+        return allImages
     }
-    val text: LiveData<String> = _text
 }
